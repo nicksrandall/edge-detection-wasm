550c412145019307b7a4b9307cab0df91d3044ad
Nick Randall <nicksrandall@gmail.com>
2019-04-12 16:37:11 -0400

    [NR] update to use web target

--- www/bootstrap.js
+++ www/bootstrap.js
@@ -1,46 +1,45 @@
-import 'edge-detection-wasm';
-import wasmURL from 'edge-detection-wasm/edge_detection_wasm_bg.wasm';
+import init, { detect } from "edge-detection-wasm";
+import wasmURL from "edge-detection-wasm/edge_detection_wasm_bg.wasm";
 
-let wasm;
-const canvas = document.getElementById('canvas');
-const videoEl = document.getElementById('video');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const videoEl = document.getElementById("video");
+const ctx = canvas.getContext("2d");
 
 function tick() {
   ctx.drawImage(videoEl, 0, 0, canvas.width, canvas.height);
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  console.time('edge::detect');
-  const data = wasm_bindgen.detect(
+  console.time("edge::detect");
+  const data = detect(
     imageData.data,
     canvas.width,
     canvas.height,
     0xff9e24ff,
-    false,
+    false
   );
-  console.timeEnd('edge::detect');
+  console.timeEnd("edge::detect");
   ctx.putImageData(new ImageData(data, canvas.width, canvas.height), 0, 0);
   window.requestAnimationFrame(tick);
 }
 
 videoEl.addEventListener(
-  'loadeddata',
+  "loadeddata",
   () => {
-    console.log('video loaded');
+    console.log("video loaded");
     window.requestAnimationFrame(tick);
   },
-  false,
+  false
 );
 
 (async () => {
-  await wasm_bindgen(wasmURL);
+  await init(wasmURL);
   const video = {
     width: 480,
     height: 640,
-    facingMode: 'environment',
+    facingMode: "environment"
   };
 
   videoEl.srcObject = await navigator.mediaDevices.getUserMedia({
     audio: false,
-    video,
+    video
   });
 })();
